1c85bab50404971051e0969a7b2357ac0bc0c974
Jose Franco <jose.franco@semperti.com>
2020-09-01 20:15:21 -0300

    last changes

--- bin/setup_prod.sh
+++ bin/setup_prod.sh
@@ -14,21 +14,21 @@ oc policy add-role-to-group system:image-puller system:serviceaccounts:${GUID}-t
 oc policy add-role-to-user edit system:serviceaccount:${GUID}-jenkins:jenkins -n ${GUID}-tasks-prod
 
 # Set up Blue Application
-#oc apply -f manifests/tasks-cm-blue.yaml -n ${GUID}-tasks-prod
-#oc apply -f manifests/tasks-dc-blue.yaml -n ${GUID}-tasks-prod
-#oc apply -f manifests/tasks-svc-blue.yaml -n ${GUID}-tasks-prod
+oc apply -f manifests/tasks-cm-blue.yaml -n ${GUID}-tasks-prod
+oc apply -f manifests/tasks-dc-blue.yaml -n ${GUID}-tasks-prod
+oc apply -f manifests/tasks-svc-blue.yaml -n ${GUID}-tasks-prod
 
-cat manifests/tasks-cm-blue.yaml | sed  "s/GUID/${GUID}/g" | oc apply -f - -n ${GUID}-tasks-prod
-cat manifests/tasks-dc-blue.yaml | sed  "s/GUID/${GUID}/g" | oc apply -f - -n ${GUID}-tasks-prod
-cat manifests/tasks-svc-blue.yaml | sed  "s/GUID/${GUID}/g" | oc apply -f - -n ${GUID}-tasks-prod
+#cat manifests/tasks-cm-blue.yaml | sed  "s/GUID/${GUID}/g" | oc apply -f - -n ${GUID}-tasks-prod
+#cat manifests/tasks-dc-blue.yaml | sed  "s/GUID/${GUID}/g" | oc apply -f - -n ${GUID}-tasks-prod
+#cat manifests/tasks-svc-blue.yaml | sed  "s/GUID/${GUID}/g" | oc apply -f - -n ${GUID}-tasks-prod
 # Set up Green Application
-#oc apply -f manifests/tasks-cm-green.yaml -n ${GUID}-tasks-prod
-#oc apply -f manifests/tasks-dc-green.yaml -n ${GUID}-tasks-prod
-#oc apply -f manifests/tasks-svc-green.yaml -n ${GUID}-tasks-prod
+oc apply -f manifests/tasks-cm-green.yaml -n ${GUID}-tasks-prod
+oc apply -f manifests/tasks-dc-green.yaml -n ${GUID}-tasks-prod
+oc apply -f manifests/tasks-svc-green.yaml -n ${GUID}-tasks-prod
 
-cat manifests/tasks-cm-green.yaml| sed  "s/GUID/${GUID}/g" | oc apply -f - -n ${GUID}-tasks-prod
-cat manifests/tasks-dc-green.yaml | sed  "s/GUID/${GUID}/g" | oc apply -f - -n ${GUID}-tasks-prod
-cat manifests/tasks-svc-green.yaml | sed  "s/GUID/${GUID}/g" | oc apply -f - -n ${GUID}-tasks-prod
+#cat manifests/tasks-cm-green.yaml| sed  "s/GUID/${GUID}/g" | oc apply -f - -n ${GUID}-tasks-prod
+#cat manifests/tasks-dc-green.yaml | sed  "s/GUID/${GUID}/g" | oc apply -f - -n ${GUID}-tasks-prod
+#cat manifests/tasks-svc-green.yaml | sed  "s/GUID/${GUID}/g" | oc apply -f - -n ${GUID}-tasks-prod
 # Expose Green service as route -> Force Green -> Blue deployment on first run
-#oc apply -f manifests/tasks-route-prod.yaml -n ${GUID}-tasks-prod
-cat manifests/tasks-route-prod.yaml | sed  "s/GUID/${GUID}/g" | oc apply -f - -n ${GUID}-tasks-prod
+oc apply -f manifests/tasks-route-prod.yaml -n ${GUID}-tasks-prod
+#cat manifests/tasks-route-prod.yaml | sed  "s/GUID/${GUID}/g" | oc apply -f - -n ${GUID}-tasks-prod
