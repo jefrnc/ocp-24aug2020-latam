d0e228844e8b85237f8bc055b1d90b05b28938f4
Jose Franco <jose.franco@semperti.com>
2020-09-01 20:32:19 -0300

    last changes

--- bin/setup_dev.sh
+++ bin/setup_dev.sh
@@ -13,11 +13,11 @@ echo "Setting up Tasks Development Environment in project ${GUID}-tasks-dev"
 oc policy add-role-to-user edit system:serviceaccount:${GUID}-jenkins:jenkins -n ${GUID}-tasks-dev
 
 # Set up Dev Application
-oc apply -f manifests/tasks-cm-dev.yaml -n ${GUID}-tasks-dev
-oc apply -f manifests/tasks-dc-dev.yaml -n ${GUID}-tasks-dev
-oc apply -f manifests/tasks-svc-dev.yaml -n ${GUID}-tasks-dev
-oc apply -f manifests/tasks-route-dev.yaml -n ${GUID}-tasks-dev
+oc apply -f ./manifests/tasks-cm-dev.yaml -n ${GUID}-tasks-dev
+oc apply -f ./manifests/tasks-dc-dev.yaml -n ${GUID}-tasks-dev
+oc apply -f ./manifests/tasks-svc-dev.yaml -n ${GUID}-tasks-dev
+oc apply -f ./manifests/tasks-route-dev.yaml -n ${GUID}-tasks-dev
 
 # Set up Dev Build Config
-oc apply -f manifests/tasks-is-dev.yaml -n ${GUID}-tasks-dev
-oc apply -f manifests/tasks-bc-dev.yaml -n ${GUID}-tasks-dev
+oc apply -f ./manifests/tasks-is-dev.yaml -n ${GUID}-tasks-dev
+oc apply -f ./manifests/tasks-bc-dev.yaml -n ${GUID}-tasks-dev
