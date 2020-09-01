e360ccd8c48025f568f15b8af087ab7301deb28d
Jose Franco <jefranco@yandex.com>
2020-09-01 09:09:09 -0300

    jenkins ok

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -12,31 +12,41 @@ REPO=$2
 CLUSTER=$3
 echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cluster ${CLUSTER}"
 
+
+ 
 # Set up Jenkins with sufficient resources
 # DO NOT FORGET TO PASS '-n ${GUID}-jenkins to ALL commands!!'
 # You do not want to set up things in the wrong project.
 
-# TBD
 buffout=$(oc get dc jenkins --ignore-not-found=true -n ${GUID}-jenkins | grep -v NAME | awk '{print $1}')
 
 echo "Start Jenkins..."
 if [[ -z ${buffout} ]];then 
-  oc new-app --name=jenkins --template=jenkins-persistent --param ENABLE_OAUTH=true --param VOLUME_CAPACITY=4Gi -n ${GUID}-jenkins
+  oc new-app --name=jenkins --template=jenkins-persistent --param ENABLE_OAUTH=true --param VOLUME_CAPACITY=2Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true -n ${GUID}-jenkins
 fi
 while (true); do 
   buffout=$(oc get dc jenkins -n ${GUID}-jenkins | grep -v NAME | awk '{print $1}')
   if [[ ! -z ${buffout} ]]; then
-    oc set resources dc jenkins --limits=memory=3Gi,cpu=2 --requests=memory=2Gi,cpu=1 -n ${GUID}-jenkins
+    oc set resources dc jenkins --limits=memory=2Gi,cpu=2 --requests=memory=2Gi,cpu=1 -n ${GUID}-jenkins
     break
   fi
   sleep 10
 done
 
+#TODO: Test in OpenTLC Environment
 
 # Create custom agent container image with skopeo.
 # Build config must be called 'jenkins-agent-appdev' for the test below to succeed
 
-# TBD
+ 
+
+ocpbuilds=$(oc get build | grep -c "jenkins-agent-appdev.*Complete")
+if [[ $ocpbuilds -eq 0 ]]; then
+    export JENKINS_AGENT=jenkins-agent-appdev
+      oc new-build  -D $'FROM docker.io/openshift/jenkins-agent-maven-35-centos7:v3.11\n
+        USER root\nRUN yum -y install skopeo && yum clean all\n
+        USER 1001' --name=jenkins-agent-appdev -n ${GUID}-jenkins
+fi
 
 
 
@@ -47,16 +57,38 @@ done
 
 # TBD
 
-#REPO_USER=$1
-#REPO_PASS=$2
+oc create secret generic gitea-secret --from-literal=username=jose.franco-semperti.com --from-literal=password=redhat123! -n ${GUID}-jenkins
 
 
 # Create pipeline build config pointing to the ${REPO} with contextDir `openshift-tasks`
 # Build config has to be called 'tasks-pipeline'.
 # Make sure you use your secret to access the repository
 
-# TBD
+# TBD ***
 
+echo "apiVersion: v1
+items:
+- kind: "BuildConfig"
+  apiVersion: "v1"
+  metadata:
+    name: "tasks-pipeline"
+  spec:
+    source:
+      type: "Git"
+      git:
+        uri: ${REPO}
+      contextDir: "openshift-tasks"
+    strategy:
+      type: "JenkinsPipeline"
+      jenkinsPipelineStrategy:
+        jenkinsfilePath: Jenkinsfile
+kind: List
+metadata: []" | oc create -f - -n ${GUID}-jenkins
+
+
+oc set build-secret --source bc/tasks-pipeline gitea-secret -n ${GUID}-jenkins
+
+# *****
 
 
 # Set up ConfigMap with Jenkins Agent definition
