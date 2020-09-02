d80981bccbee7c5035908b825a76024aa6d034ca
Jose Franco <jose.franco@semperti.com>
2020-09-01 21:58:53 -0300

    last changes

--- bin/setup_prod.sh
+++ bin/setup_prod.sh
@@ -14,14 +14,14 @@ oc policy add-role-to-group system:image-puller system:serviceaccounts:${GUID}-t
 oc policy add-role-to-user edit system:serviceaccount:${GUID}-jenkins:jenkins -n ${GUID}-tasks-prod
 
 # Set up Blue Application
-# oc apply -f manifests/tasks-cm-blue.yaml -n ${GUID}-tasks-prod
-# oc apply -f manifests/tasks-dc-blue.yaml -n ${GUID}-tasks-prod
-# oc apply -f manifests/tasks-svc-blue.yaml -n ${GUID}-tasks-prod
+oc apply -f manifests/tasks-cm-blue.yaml -n ${GUID}-tasks-prod
+oc apply -f manifests/tasks-dc-blue.yaml -n ${GUID}-tasks-prod
+oc apply -f manifests/tasks-svc-blue.yaml -n ${GUID}-tasks-prod
 
 # Set up Green Application
-# oc apply -f manifests/tasks-cm-green.yaml -n ${GUID}-tasks-prod
-# oc apply -f manifests/tasks-dc-green.yaml -n ${GUID}-tasks-prod
-# oc apply -f manifests/tasks-svc-green.yaml -n ${GUID}-tasks-prod
+oc apply -f manifests/tasks-cm-green.yaml -n ${GUID}-tasks-prod
+oc apply -f manifests/tasks-dc-green.yaml -n ${GUID}-tasks-prod
+oc apply -f manifests/tasks-svc-green.yaml -n ${GUID}-tasks-prod
 
 # Expose Green service as route -> Force Green -> Blue deployment on first run
-# oc apply -f manifests/tasks-route-prod.yaml -n ${GUID}-tasks-prod
+oc apply -f manifests/tasks-route-prod.yaml -n ${GUID}-tasks-prod
